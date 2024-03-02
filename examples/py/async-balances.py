2276141531730a03164eae98493b7c252b514dc0
Egor <68647780+w0terme10n@users.noreply.github.com>
2024-03-02 17:21:44 +0300

    basic fix

--- examples/py/async-balances.py
+++ examples/py/async-balances.py
@@ -3,28 +3,24 @@
 import asyncio
 import os
 import sys
-
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
 
-import ccxt.async_support as ccxt  # noqa: E402
-
+import ccxt.async_support as ccxt  # noqa E402
 
 async def test(exchange):
     print(await exchange.fetch_balance())
     await exchange.close()
 
-
 kraken = ccxt.kraken({
     'apiKey': "YOUR_API_KEY",
     'secret': "YOUR_SECRET",
-    'verbose': True,  # switch it to False if you don't want the HTTP log
+    'verbose': True  # switch it to False if you don't want the HTTP log
 })
 bitfinex = ccxt.bitfinex({
     'apiKey': "YOUR_API_KEY",
     'secret': "YOUR_SECRET",
-    'verbose': True,  # switch it to False if you don't want the HTTP log
-})
+    'verbose': True  # switch it to False if you don't want the HTTP log})
 
 [asyncio.ensure_future(test(exchange)) for exchange in [kraken, bitfinex]]
 pending = asyncio.Task.all_tasks()
