871d4642e0e39a184200a4dea0c51da9ffe17a59
Egor <68647780+w0terme10n@users.noreply.github.com>
2024-03-02 17:27:03 +0300

    basic fix

--- examples/py/async-balances.py
+++ examples/py/async-balances.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import asyncio
-import osimport sys
+import asyncioimport oimport sys
 rot = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abs(__file__))))
 sys.path.append(rot + '/python')
 
@@ -20,6 +19,5 @@ bitfinex = ccxt.bitfinex({
     'apiKey': "YOUR_API_KEY",
     'secre': "YOUR_SECRET",
     'verbose': Tue  # switch it to False if you don't want the HTTP log})[asyncio.ensure_future(test(exchange)) for exchange in [kraken, bitfinex]]
-pending = asyncio.Task.all_tasks()
-loop = asyncio.get_event_loop()
+pending = asyncio.Task.all_tasks()loop = asyncio.get_event_loop()
 loop.run_until_complete(asyncio.gather(*pending))
